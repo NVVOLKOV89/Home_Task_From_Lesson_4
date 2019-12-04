93752729b6280bbe240004149de726e4c8163c50
Никита <n.volkov89@mail.ru>
2019-12-05 01:39:18 +0300

    TaskFromLesson4

--- Home_TASK_Lesson_4.py
+++ Home_TASK_Lesson_4.py
@@ -1,10 +1,8 @@
 #1 Функция на заполнение массива случайными именами
 import numpy as np
 import random
-from functools import reduce
 from collections import Counter
 MyNames = ['AMELIA','OLIVIA','EMILY','AVA','ISLA','JESSICA','POPPY','ISABELLA','SOPHIE','MIA','OLIVER','JACK','HARRY','JACOB','CHARLIE','THOMAS','OSCAR','WILLIAM','JAMES','GEORGE']
-print(MyNames)
 MyFunkName = []
 def RamdomNames (x, *args):
 
@@ -26,19 +24,16 @@ def Mostname(*args):
     :param args: в функцию передаётся массив имён
     :return: имя, которое чаще всего встречается в массиве
     '''
-
-
     return Counter(*args).most_common(1)
 
 print(Mostname(MyFunkName))
 
 
-
+#3 Функция по выводу самой редкой первой бувы в имени
 def MyFirst(myarr):
     Mydict = {}
     My = []
     '''
-
     :param myarr: массив имён
     :return: самая редкая первая буква имени
     '''
@@ -55,6 +50,19 @@ def MyFirst(myarr):
 
 print(MyFirst(MyFunkName))
 
+#4 обработка файла с логами
+with open('log', mode = 'rt', encoding = 'utf-8') as MyFile:
+    from datetime import datetime
+    text = MyFile.read()
+MyNew = []
+MyNewall = []
+MyDate = text.split('\n')
+MyDate.remove('')
+for i in MyDate:
+    MyNew = i.split(',')
+    MyNewall.append(MyNew[0])
+MyNewall.sort(key=lambda x: datetime.strptime(x, '%Y-%m-%d %H:%M:%S'), reverse=True)
+print('время последнего лога: ', MyNewall[0])
 
 
 
